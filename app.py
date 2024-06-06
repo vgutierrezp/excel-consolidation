e675c477d5e8eddde4f9e72af7ab8c11e286d2aa
vgutierrezp <victor.gutierrez@ngr.com.pe>
2024-06-05 22:55:13 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,29 +1,16 @@
 import streamlit as st
 import pandas as pd
-import os
 
-# Función para cargar los datos
+# Cargar el archivo consolidado desde el repositorio
 @st.cache_data
 def load_data():
+    file_path = 'consolidated_file.xlsx'
     try:
-        # Ruta para el entorno local
-        file_path_local = r'C:\Users\vgutierrez\chatbot_project\consolidated_file.xlsx'
-        # Ruta para el entorno de despliegue
-        file_path_deploy = 'consolidated_file.xlsx'
-
-        if os.path.exists(file_path_local):
-            file_path = file_path_local
-        else:
-            file_path = file_path_deploy
-
-        st.write(f"Intentando cargar el archivo desde: {file_path}")  # Mensaje para depuración
         data = pd.read_excel(file_path)
         return data
-    except FileNotFoundError as e:
-        st.error(f"El archivo no se encontró: {e}")
     except Exception as e:
-        st.error(f"Ocurrió un error al cargar los datos: {e}")
-    return None
+        st.error(f"Error al cargar los datos: {e}")
+        return pd.DataFrame()
 
 # Función principal
 def main():
@@ -31,10 +18,12 @@ def main():
 
     data = load_data()
 
-    if data is not None:
-        st.write(data)
-    else:
+    if data.empty:
         st.error("No se pudieron cargar los datos.")
+        return
+
+    # Mostrar los datos
+    st.write(data)
 
 if __name__ == "__main__":
     main()
