f7f0ab3232764a826cfbc4498f3c03cb133944e4
vgutierrezp <victor.gutierrez@ngr.com.pe>
2024-06-08 00:42:43 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 from io import BytesIO
-import os
 from datetime import datetime, timedelta
 
 # Cargar el archivo consolidado desde el repositorio
@@ -32,18 +31,26 @@ def generate_excel_with_dates(df, store_name):
 
     for index, row in new_df.iterrows():
         freq = row['Frecuencia']
-        current_date = pd.to_datetime(row['Ult. Prev.'], format='%d/%m/%y')
+        current_date = pd.to_datetime(row['Ult. Prev.'], format='%d/%m/%Y')
         col_num = 1
         while current_date <= max_date:
             new_df.loc[index, f'Prog.{col_num}'] = current_date.strftime('%d/%m/%Y')
             current_date += timedelta(days=freq)
             col_num += 1
 
+    new_df = new_df.dropna(subset=['Ult. Prev.'])
+    new_df['Unique_Service'] = new_df['Familia'] + new_df['Tipo de Equipo'] + new_df['Tipo de Servicio'] + new_df['Ejecutor'] + new_df['Frecuencia'].astype(str)
+    new_df['Ult. Prev.'] = pd.to_datetime(new_df['Ult. Prev.'], format='%d/%m/%Y')
+    new_df = new_df.loc[new_df.groupby('Unique_Service')['Ult. Prev.'].idxmax()]
+
     with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
         worksheet_name = 'Fechas Planificadas'
         new_df.to_excel(writer, index=False, sheet_name=worksheet_name, startrow=2)
         worksheet = writer.sheets[worksheet_name]
         worksheet.write('A1', f'PLAN ANUAL DE MANTENIMIENTO DE LA TIENDA: {store_name}')
+        bold = writer.book.add_format({'bold': True})
+        worksheet.set_row(0, None, bold)
+        worksheet.set_column('H:H', None, writer.book.add_format({'num_format': 'dd/mm/yyyy'}))
     processed_data = output.getvalue()
     return processed_data
 
@@ -108,17 +115,21 @@ def main():
             mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
         )
 
-    # Botón para generar el Excel con fechas calculadas y descargarlo
+    # Botón para generar el Excel con fechas calculadas
     if selected_store:
-        planned_excel_data = generate_excel_with_dates(filtered_data, selected_store)
-        st.sidebar.download_button(
-            label='Programa Anual de Mantenimiento',
-            data=planned_excel_data,
-            file_name='programa_anual_mantenimiento.xlsx',
-            mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-        )
+        if st.sidebar.button('Programa Anual de Mantenimiento'):
+            if selected_month or selected_brand or selected_family:
+                st.sidebar.warning("Por favor, deje solo el filtro de tienda lleno.")
+            else:
+                planned_excel_data = generate_excel_with_dates(filtered_data, selected_store)
+                st.sidebar.download_button(
+                    label='Descargar Programa Anual',
+                    data=planned_excel_data,
+                    file_name='programa_anual_mantenimiento.xlsx',
+                    mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+                )
     else:
-        st.sidebar.error("Por favor, seleccione una tienda.")
+        st.sidebar.warning("Por favor, seleccione una tienda.")
 
 if __name__ == "__main__":
     main()
