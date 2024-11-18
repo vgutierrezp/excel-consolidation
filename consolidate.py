b8435492847997f801c7a590613d6a321e666637
vgutierrezp <victor.gutierrez@ngr.com.pe>
2024-11-17 22:27:37 -0500

    Add files via upload

--- consolidate.py
+++ consolidate.py
@@ -21,14 +21,19 @@ def consolidate_excels(source_folder, output_file):
                     df['Hoja'] = sheet_name
                     df['Fila'] = df.index + 2  # Asumiendo que el DataFrame original empieza en la fila 2 del archivo Excel
 
-                    all_data = pd.concat([all_data, df], ignore_index=True)
+                    # Verifica si el DataFrame no está vacío o contiene solo valores NA
+                    if not df.empty and df.dropna(how='all').shape[1] > 0:
+                        all_data = pd.concat([all_data, df], ignore_index=True)
 
     # Guardar el archivo consolidado
-    all_data.to_excel(output_file, index=False)
+    if not all_data.empty:
+        all_data.to_excel(output_file, index=False)
+        print("Consolidación completada.")
+    else:
+        print("No se encontró información para consolidar.")
 
 if __name__ == "__main__":
     # Asegurarse de que la ruta de la carpeta de proveedores es correcta
     source_folder = r"C:\Users\vgutierrez\OneDrive - Servicios Compartidos de Restaurantes SAC\Documentos\01 Plan Preventivo Anual NGR\Preventivo\2024 PAM\PROVEEDORES"
     output_file = r"C:\Users\vgutierrez\chatbot_project\consolidated_file.xlsx"
     consolidate_excels(source_folder, output_file)
-    print("Consolidación completada.")
